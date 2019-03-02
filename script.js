ecc29d6d221bf8a02f798102a514d08352b41980
Sairath Bhattacharjya <sairath@gmail.com>
2019-03-02 11:20:39 -0600

    added code for dining

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function onLoad() {
     name = name ? name.trim().split('=')[1] : '';
     document.getElementById('name').value = name;
     setNameMessage(false);
+    setDineOptions();
 }
 
 function setNameMessage(shouldSetCookie) {
@@ -30,7 +31,45 @@ function _dine() {
     var message = '';
     if (hour < 11) message = 'breakfast';
     else if (hour >= 11 && hour < 15) message = 'lunch';
-    else if (hour >= 15 && hour < 18) message = 'evening snacks';
+    else if (hour >= 15 && hour < 18) message = 'evening-snacks';
     else if (hour >= 18) message = 'dinner';
     return message;
+}
+
+function setDinePreferance() {
+    var choice = document.getElementById('choice').value;
+    var expires = new Date();
+    expires.setMonth(expires.getMonth() + 12);
+    document.cookie = "currentChoice=" + choice + ";expires=" + expires + ";path=/";
+    var dine = _dine();
+    var cookies = document.cookie.split(';');
+    var options = cookies.find(c => { return c.trim().startsWith(dine) });
+    options = options ? JSON.parse(options.trim().split('=')[1]) : [];
+    var index = options.indexOf(choice);
+    if (index == -1) {
+        options.push(choice);
+    }
+    document.cookie = `${dine}=${JSON.stringify(options)};expires=${expires};path=/`;
+    document.getElementById('foodPanel').setAttribute('style', 'display: none');
+    document.getElementById('currentDineLabel').innerHTML = choice;
+    document.getElementById('orderPanel').setAttribute('style', 'display: block');
+}
+
+function setDineOptions() {
+    var dine = _dine();
+    var cookies = document.cookie.split(';');
+    var options = cookies.find(c => { return c.trim().startsWith(dine) });
+    options = options ? JSON.parse(options.trim().split('=')[1]) : [];
+    var optionHtml = '';
+    options.forEach(option => {
+        optionHtml += `<a href="#" onclick="document.getElementById('choice').value='${option}'" class="list-group-item">${option}</a>`
+    })
+    document.getElementById('options').innerHTML = optionHtml;
+}
+
+function deleteAllCookies() {
+    var c = document.cookie.split("; ");
+    for (i in c)
+        document.cookie = /^[^=]+/.exec(c[i])[0] + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    location.reload();
 }
