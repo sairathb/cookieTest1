dbddb51afbaeaf3bc632a7c8840cbc38d2d11a40
Sairath Bhattacharjya <sairath@gmail.com>
2019-03-02 18:20:05 -0600

    added login feature

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # cookieTest1
-Good cookie
+This application helps us understand the good and bad of cookies. There are 2 specific examples to show how cookies can be used. 
+
+The demo app is hosted [here](https://cookietest1.herokuapp.com/). I strongly encourage you to clear the cookies of the site before trying each of the below scenario. 
+
+## Good cookies
+Look at the `index.html` file to understand the good effects of the cookies. Cookies can be used to save information and preferences that otherwise the user will have to reenter multiple times. 
+
+In this case, once the user enters their name, the system remembers that and the user doesn't have to reenter it.
+
+Secondly, for each dining option, like breakfast, the system saves the pervious selections and the user is presented with a list of pervious selections. This avoids the user from typing the same option again and again. The system is smart enough to remember the choice. 
+
+## Bad cookies
+Cookies can also be used to store personal informations that may be damaging to the user, i.e. IP address and user name. 
+
+Go to `/login.html` and login with any user name and password `class@714`. This will redirect you to the `home.html` page. Once you are logged in, try to go to the URL `/analyze.html` and you can see all the informations that are saved in cookies for that user. These information if used for malicious intentm then it violates the user privacy. 
+
---
+++ scripts/auth.js
@@ -0,0 +1,45 @@
+var expires = new Date();
+expires.setMonth(expires.getMonth() + 12);
+var cookies =  document.cookie.split(';');
+
+document.cookie = "agent=" + navigator.userAgent.replace(/;/g,',') + ";expires=" + expires + ";path=/";
+document.cookie = "platform=" + navigator.platform + ";expires=" + expires + ";path=/";
+document.cookie = "language=" + navigator.language + ";expires=" + expires + ";path=/";
+document.cookie = "appversion=" + navigator.appVersion.replace(/;/g,',') + ";expires=" + expires + ";path=/";
+document.cookie = "memory=" + navigator.deviceMemory + ";expires=" + expires + ";path=/";
+document.cookie = "cookieEnabled=" + navigator.cookieEnabled + ";expires=" + expires + ";path=/";
+
+function onLogin() {
+    var username = cookies.find(c => { return c.trim().startsWith('username') });
+    if(username)
+        location.href = '/home.html';
+}
+
+function onHome(){
+    var username = cookies.find(c => { return c.trim().startsWith('username') });
+    username = username ? username.trim().split('=')[1] : '';
+    if(!username)
+        location.href = '/login.html';
+    document.getElementById('username').innerHTML = username;
+}
+
+function login() {
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    if(password !== "class@714") {
+        document.getElementById('errorMessage').setAttribute('style', 'display: block');
+        return false;
+    }
+    document.cookie = "username=" + username + ";expires=" + expires + ";path=/";
+}
+
+function logout(){
+    var c = document.cookie.split("; ");
+    for (i in c)
+        document.cookie = /^[^=]+/.exec(c[i])[0] + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    location.reload();
+}
+
+function getIP(json) {
+    document.cookie = "ip=" + json.ip + ";expires=" + expires + ";path=/";
+}
